a04e96e571d69d927b4270417a1d811cc9d1eb3e
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2025-09-08 17:31:02 -0400

    semantic components + styles

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <section className='doormat-nav-container'>
+        <img src='/Assets/icons_assets/Logo.svg' alt='' />
+        <div className='doormat-nav'>
+          <h2>Doormat Navigation</h2>
+          <nav>
+            <ul>
+              <li><a href='#home'>Home</a></li>
+              <li><a href='#about'>About</a></li>
+              <li><a href='#menu'>Menu</a></li>
+              <li><a href='#reservations'>Reservations</a></li>
+              <li><a href='#order-online'>Order Online</a></li>
+            </ul>
+          </nav>
+        </div>
+
+      </section>
+      <section className='contact'>
+        <h2>Contact</h2>
+        <ul>
+          <li>Phone Number</li>
+          <li>Email</li>
+          <li>Address</li>
+        </ul>
+      </section>
+      <section className='social-media'>
+        <h2>Social Media</h2>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Tik Tok</li>
+          <li>X</li>
+        </ul>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './header.css'
+import Navbar from './Navbar'
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <header>
+      <img src='/Assets/icons_assets/Logo.svg' alt='little lemon logo' />
+      <img onClick={() => setIsOpen(!isOpen)} className='hamburguer-icon' src='/Assets/icons_assets/icon_hamburger_menu.svg' alt='little lemon logo' />
+
+      <Navbar isOpen={isOpen} />
+    </header>
+
+  )
+}
+
+export default Header
