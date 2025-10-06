d894fb67195602a421f3f8aecf3fbd578c08d367
Asdrubal Aldana <asdrubal.aldana12@gmail.com>
2025-10-05 20:30:30 -0400

    fix

--- src/components/HighlightsSection/HighlightsSection.js
+++ src/components/HighlightsSection/HighlightsSection.js
@@ -6,21 +6,21 @@ const HighlightsSection = ({ navigate }) => {
   const dishes = [
     {
       id: 1,
-      imgSrc: './Assets/icons_assets/greek_salad.jpg',
+      imgSrc: 'Assets/icons_assets/greek_salad.jpg',
       title: 'Greek Salad',
       price: 12.99,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi accusantium iste et dicta recusandae in magnam. Eius, saepe. Cumque illum error aut dicta nisi eos itaque ab laboriosam sit?'
     },
     {
       id: 2,
-      imgSrc: './Assets/icons_assets/Bruchetta.svg',
+      imgSrc: 'Assets/icons_assets/Bruchetta.svg',
       title: 'Bruchetta',
       price: 10.99,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi accusantium iste et dicta recusandae in magnam. Eius, saepe. Cumque illum error aut dicta nisi eos itaque ab laboriosam sit?'
     },
     {
       id: 3,
-      imgSrc: './Assets/icons_assets/lemon_dessert.jpg',
+      imgSrc: 'Assets/icons_assets/lemon_dessert.jpg',
       title: 'Lemon Dessert',
       price: 9.99,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus commodi accusantium iste et dicta recusandae in magnam. Eius, saepe. Cumque illum error aut dicta nisi eos itaque ab laboriosam sit?'
